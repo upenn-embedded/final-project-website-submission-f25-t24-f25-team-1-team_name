e92a139a397c0f9edbe68bb9da863ae316e75617
Bowen Wang <123225973+Bowen-wang-sps@users.noreply.github.com>
2025-12-09 23:20:04 -0500

    Add files via upload

---
+++ ESP32_lichess/network_stuff.h
@@ -0,0 +1,8 @@
+#ifndef NETWORK_STUFF_H
+#define NETWORK_STUFF_H
+
+#define WIFI_SSID "iFhone"
+#define WIFI_PASS "4Fatredsheep"
+#define LICHESS_API_TOKEN "lip_q5WzbKshAtF8cU4vIHNh"
+#define LICHESS_USER "ayu2126"  
+#endif
